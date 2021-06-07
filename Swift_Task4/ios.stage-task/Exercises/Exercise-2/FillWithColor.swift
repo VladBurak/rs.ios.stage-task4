9466860c1700a7df8078533bcc9babe2b20602a4
Vlad Burak <byrakvlad@gmail.com>
2021-06-07 18:04:53 +0300

    feat: tasks 1 and 2

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,34 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+
+        if (row < 0 || column < 0 || row >= image.count || column >= image[0].count || image.count < 0 || newColor > 65536 || image[0].count > 50) {
+            return image
+        }
+        
+//        let m = image.count
+//        let n = image[0].count
+        let color = image[row][column]
+        var matrix = image
+        matrica(&matrix, row, column, newColor,color)
+        return matrix
+    }
+    
+    func matrica(_ matrix: inout [[Int]], _ row: Int, _ column: Int, _ newColor: Int, _ color: Int) {
+        if (row >= 0 && column >= 0 && row < matrix.count && column < matrix[0].count && matrix[row][column] != newColor && matrix[row][column] == color) {
+            
+            if (column < matrix[row].count) {
+            matrix[row][column] = newColor
+            // top
+            matrica(&matrix, row - 1, column, newColor, color)
+            // bottom
+            matrica(&matrix, row + 1, column, newColor, color)
+            // left
+            matrica(&matrix, row, column - 1, newColor, color)
+            // right
+            matrica(&matrix, row, column + 1, newColor, color)
+
+            }
+        }
     }
 }
